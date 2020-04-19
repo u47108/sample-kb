649b4f95881a7e0e8d5c68d02b5d34c6926885a5
luxo <LeonardoSilva.Zenta@latam.com>
2020-04-19 18:34:35 -0400

    ultimos cambios y README actualizado

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# sample-kb
+# Una API Restful de Farmacias en Turno Público 
+El proyecto se separa en capas BE microservicios de lógica y consulta a otras APIS en el caso propuesto las APIS no eran con seguridad si se necesitara el uso del x-api-key se deberia utilizar secretos para guardar las claves.
+
+El microservicio BFF interactua directamente con el Front FE, por lo que se configuraron los filtros CORS.
+El front FE no está completo,nunca tuve una API de google maps para habilitar, no me desgaste en eso.
+
+Mi primer enfoque pensé en usar la cuenta que tenia en Kubernates pero se me vencieron los $300 dolares y para habilitar otra cuenta de facturación esta lenta la cosa con el tema del covid-19 al momento de escribir este manual aún no me habilitan. 
+
+Los Servicios REST (API),están basados en Spring Boot
+
+## Built With
+
+* [Maven](https://maven.apache.org/) - Dependency Management
+    
+* mvn clean install
+
+* java -jar target/*.jar
+
+## Instalación
+
+Se deben configurar las siguientes variables de entorno proyecto BE:
+
+- **COMUNAS_ENDPOINT**: Endpoint del minsal(ej: *https://midastest.minsal.cl/farmacias/maps/index.php/utilidades/maps_obtener_comunas_por_regiones*)
+- **COMUNAS_PARAM_NAME**: PARAMETRO  (*reg_id*)
+- **COMUNAS_PARAM_VALUE**: PARAMETRO valor (*7*)
+- **WS_TIMEOUT**: Tiempo de espera en milisegundos de conexión y operación con servicios externos (*15000*)
+
+## Documentacion API
+
+*http://localhost:8080/v2/api-docs*
+
+## Deployment
+kubectl create -f deploy-demo-bff.yaml
+
+kubectl describe deployment/deploy-demo-bff
+kubectl describe pod -l app=demo-bff
+
+## Service
+kubectl create -f aplicacion.yaml
+
+kubectl describe service demo-bff
+
+minikube service demo-bff
+
+## Test 
+
+curl $(minikube ip):${NodePort}
+
+e.g. curl http://192.168.99.100:30658
+
+```
+
--- demo-fe/src/main/java/com/famacias/turno/publico/demofe/controller/PublicController.java
+++ demo-fe/src/main/java/com/famacias/turno/publico/demofe/controller/PublicController.java
@@ -1,19 +1,26 @@
 package com.famacias.turno.publico.demofe.controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
 
 import com.famacias.turno.publico.demofe.domain.FarmaciaResponse;
+import com.famacias.turno.publico.demofe.domain.ResponseFarmacias;
 import com.famacias.turno.publico.demofe.domain.SearchRequest;
 
 @Controller
@@ -21,6 +28,8 @@ public class PublicController {
 
     @Value("${maps.apikey}")
     private String apimaps;
+    @Value("${bff.endpoint}")
+    private String endpoint;
 
     @GetMapping("/viewmaps")
     public String index(Model model) {
@@ -38,8 +47,22 @@ public class PublicController {
             farmacia.setLongitud("-70.735941");
             farmacia.setNombreLocal("AHUMADA");
             List<FarmaciaResponse> listGeo = new ArrayList<>();
-            model.addAttribute("listGeo", listGeo);
+            final String uri = endpoint;
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+            HttpEntity<?> entity = new HttpEntity<>(searchRq,getHttpHeaders());
+            ResponseFarmacias response = restTemplate.postForObject(this.endpoint, entity, ResponseFarmacias.class);
+            if(response!=null && !response.getFarmacias().isEmpty()) {
+            model.addAttribute("listGeo", response.getFarmacias());
+            }
         }
         return ((errors.hasErrors()) ? "home" : "maps");
+    }
+    private static HttpHeaders getHttpHeaders() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        httpHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        return httpHeaders;
     }
 }
